5db59b58f1269ccbb2590c9aa4a8175b35d1fcd4
Ahmdabusamra <ahmdabusamra@gmail.com>
2025-10-19 16:31:50 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,1 +1,17 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'node:path'
+
+// Vite config to ensure proper building
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'), // Alias the src directory
+    },
+  },
+  build: {
+    outDir: 'dist', // Specify the output directory for production build
+  },
+})
 
